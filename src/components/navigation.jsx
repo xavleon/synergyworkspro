b630b607bb9623a8493b09bfd5b2f03f1555b99d
Javier Leon <mrm.leon93@gmail.com>
2024-07-16 00:35:45 -0400

    updated content to cleaning business website

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            SynergyWorks <br></br> picture
+            SynergyWorks Pro <br></br> picture
           </a>{" "}
         </div>
 
