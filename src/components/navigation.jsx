3ef054ea57895ab72d6c6832be157e95d7d926dd
Javier Leon <mrm.leon93@gmail.com>
2024-07-23 23:37:47 -0400

    navigation collapse menu phone screen

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { useRef } from "react";
 
 export const Navigation = (props) => {
+  const navCollapse = useRef(null); // 2. Create reference for collapse container
+
+  const handleNavItemClick = () => {
+    // 3. Function to handle item click
+    if (window.innerWidth < 768) {
+      navCollapse.current.classList.remove("in"); // Collapse the menu
+    }
+  };
+
   return (
     <nav id="menu" className="navbar navbar-default navbar-fixed-top">
       <div className="container">
@@ -25,40 +35,69 @@ export const Navigation = (props) => {
         <div
           className="collapse navbar-collapse"
           id="bs-example-navbar-collapse-1"
+          ref={navCollapse} // 1. Add reference to collapse container
         >
           <ul className="nav navbar-nav navbar-right">
             <li>
-              <a href="#features" className="page-scroll">
+              <a
+                href="#features"
+                className="page-scroll"
+                onClick={handleNavItemClick}
+              >
                 Features
               </a>
             </li>
             <li>
-              <a href="#about" className="page-scroll">
+              <a
+                href="#about"
+                className="page-scroll"
+                onClick={handleNavItemClick}
+              >
                 About
               </a>
             </li>
             <li>
-              <a href="#services" className="page-scroll">
+              <a
+                href="#services"
+                className="page-scroll"
+                onClick={handleNavItemClick}
+              >
                 Services
               </a>
             </li>
             <li>
-              <a href="#portfolio" className="page-scroll">
+              <a
+                href="#portfolio"
+                className="page-scroll"
+                onClick={handleNavItemClick}
+              >
                 Gallery
               </a>
             </li>
             <li>
-              <a href="#testimonials" className="page-scroll">
+              <a
+                href="#testimonials"
+                className="page-scroll"
+                onClick={handleNavItemClick}
+              >
                 Testimonials
               </a>
             </li>
             <li>
-              <a href="#team" className="page-scroll">
+              <a
+                href="#team"
+                className="page-scroll"
+                onClick={handleNavItemClick}
+              >
                 Team
               </a>
             </li>
             <li>
-              <a href="#contact" className="page-scroll">
+              <a
+                href="#contact"
+                className="page-scroll"
+                onClick={handleNavItemClick}
+              >
                 Contact
               </a>
             </li>
