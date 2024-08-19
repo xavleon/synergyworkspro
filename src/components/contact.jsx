c86f23445a8f45ee0e6dfb59db195c03aad6191a
unknown <mrm.leon93@gmail.com>
2024-08-18 20:20:43 -0400

    build dependancies update

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -9,7 +9,7 @@ const initialState = {
   message: "",
 };
 export const Contact = (props) => {
-  const [{ name, email, phone, message }, setState] = useState(initialState);
+  const [{ name, email, message }, setState] = useState(initialState);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -20,10 +20,6 @@ export const Contact = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(name, email, message);
-
-    {
-      /* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */
-    }
 
     console.log(process.env);
 
